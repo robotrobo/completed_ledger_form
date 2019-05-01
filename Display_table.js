87f2778316a6ec409492d0384c54d01bc2587b29
anish agrawal <kanhaagrawal0804@gmail.com>
2019-05-01 15:41:17 +0530

    Fixing minor bigs

--- Display_table.js
+++ Display_table.js
@@ -4,107 +4,107 @@ let debugarr;
 function display(arr, name_of_comp, admin) {
     // let party_name = name_of_comp;
     // if(lab1 && lab2 && user_in && pass_in && sub_button && register_button && info_re){
-    if (!admin) {
-        lab1.remove();
-        debug = arr;
-        lab2.remove();
-        user_in.remove();
-        pass_in.remove();
-        sub_button.remove();
-        register_button.remove();
-        info_re.remove();
-        // }
-    }
-    name_head = createElement("h1", `${name_of_comp}`);
-    table = createElement("table", "<thead><tr><th>Date</th><th>Particular</th><th>Vch type</th><th>Debit</th><th>Credit</th></tr></thead>");
-    table.id("mytable");
-    table.attribute("class", "table");
-    tbl = document.getElementById("mytable");
+    // if (!admin) {
+    //     lab1.remove();
+    //     debug = arr;
+    //     lab2.remove();
+    //     user_in.remove();
+    //     pass_in.remove();
+    //     sub_button.remove();
+    //     register_button.remove();
+    //     info_re.remove();
+    //     // }
+}
+name_head = createElement("h1", `${name_of_comp}`);
+table = createElement("table", "<thead><tr><th>Date</th><th>Particular</th><th>Vch type</th><th>Debit</th><th>Credit</th></tr></thead>");
+table.id("mytable");
+table.attribute("class", "table");
+tbl = document.getElementById("mytable");
 
 
 
-    let total_debit = 0;
-    let total_credit = 0;
+let total_debit = 0;
+let total_credit = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        var row = tbl.insertRow();
-        let temp_array = arr[i].split(",") //.splice(2,1);
-        temp_array.splice(2, 1);
-        // debugarr = temp_array;
-        // console.debug(temp_array);
-        if (temp_array[0] !== "\"\"") {
-            for (let j = 0; j < temp_array.length; j++) {
-                var cell = row.insertCell();
-                if (j == 4 && temp_array[j] != '') {
-                    let amount = abs(eval(temp_array[j]));
-                    total_credit += amount;
-                }
-                if (j == 3 && temp_array[j] != '') {
-                    let amount = abs(eval(temp_array[j]));
-                    cell.innerHTML = amount;
-                    total_debit += amount
-                } else if (j == 1 && temp_array[j] == "\"(as per details)\"") {
-                    cell.innerHTML = "GST sales";
-                } else {
-                    cell.innerHTML = temp_array[j];
-                }
+for (let i = 0; i < arr.length; i++) {
+    var row = tbl.insertRow();
+    let temp_array = arr[i].split(",") //.splice(2,1);
+    temp_array.splice(2, 1);
+    // debugarr = temp_array;
+    // console.debug(temp_array);
+    if (temp_array[0] !== "\"\"") {
+        for (let j = 0; j < temp_array.length; j++) {
+            var cell = row.insertCell();
+            if (j == 4 && temp_array[j] != '') {
+                let amount = abs(eval(temp_array[j]));
+                total_credit += amount;
             }
-        } else {
-            continue;
+            if (j == 3 && temp_array[j] != '') {
+                let amount = abs(eval(temp_array[j]));
+                cell.innerHTML = amount;
+                total_debit += amount
+            } else if (j == 1 && temp_array[j] == "\"(as per details)\"") {
+                cell.innerHTML = "GST sales";
+            } else {
+                cell.innerHTML = temp_array[j];
+            }
         }
+    } else {
+        continue;
     }
-    // console.debug(total);
+}
+// console.debug(total);
 
-    row = tbl.insertRow();
+row = tbl.insertRow();
+cell = row.insertCell();
+cell.innerHTML = "";
+cell = row.insertCell();
+cell.innerHTML = "";
+cell = row.insertCell();
+cell.innerHTML = "";
+
+console.log(`total debit: ${total_debit} \n total credit: ${total_credit}`);
+let total = total_credit - total_debit;
+console.log(`total = ${total}`);
+let calc_opening = eval(close_bal) - total;
+console.log(`Calc opening = ${calc_opening}`);
+open_bal = calc_opening.toFixed(2);
+if (eval(open_bal) < 0) {
+    cell = row.insertCell();
+    cell.innerHTML = `<strong>Opening balace:${abs(eval(open_bal))}</strong>`;
+    cell = row.insertCell();
+    cell.innerHTML = "";
+} else if (eval(open_bal) > 0) {
     cell = row.insertCell();
     cell.innerHTML = "";
     cell = row.insertCell();
-    cell.innerHTML = "";
+    cell.innerHTML = `<strong>Opening balace:${abs(eval(open_bal))}</strong>`;
+}
+row = tbl.insertRow();
+cell = row.insertCell();
+cell.innerHTML = "";
+cell = row.insertCell();
+cell.innerHTML = "";
+cell = row.insertCell();
+cell.innerHTML = "";
+
+
+if (eval(close_bal) < 0) {
+    cell = row.insertCell();
+    cell.innerHTML = `<strong>Closing balace:${abs(eval(close_bal))}</strong>`;
     cell = row.insertCell();
     cell.innerHTML = "";
-
-    console.log(`total debit: ${total_debit} \n total credit: ${total_credit}`);
-    let total = total_credit - total_debit;
-    console.log(`total = ${total}`);
-    let calc_opening = eval(close_bal) - total;
-    console.log(`Calc opening = ${calc_opening}`);
-    open_bal = calc_opening.toFixed(2);
-    if (eval(open_bal) < 0) {
-        cell = row.insertCell();
-        cell.innerHTML = `<strong>Opening balace:${abs(eval(open_bal))}</strong>`;
-        cell = row.insertCell();
-        cell.innerHTML = "";
-    } else if (eval(open_bal) > 0) {
-        cell = row.insertCell();
-        cell.innerHTML = "";
-        cell = row.insertCell();
-        cell.innerHTML = `<strong>Opening balace:${abs(eval(open_bal))}</strong>`;
-    }
-    row = tbl.insertRow();
+} else if (eval(close_bal) > 0) {
     cell = row.insertCell();
     cell.innerHTML = "";
     cell = row.insertCell();
-    cell.innerHTML = "";
-    cell = row.insertCell();
-    cell.innerHTML = "";
+    cell.innerHTML = `<strong>Closing balace:${abs(eval(close_bal))}</strong>`;
+}
 
+admin_button.remove();
+reset_button.position(windowWidth / 2, table.size().height + 300);
+httpGet(`${window.location.origin}/log.php?string=${encodeURIComponent(name_of_comp)}%20logged%20in`);
+document.getElementById("dev").style.display = "none";
 
-    if (eval(close_bal) < 0) {
-        cell = row.insertCell();
-        cell.innerHTML = `<strong>Closing balace:${abs(eval(close_bal))}</strong>`;
-        cell = row.insertCell();
-        cell.innerHTML = "";
-    } else if (eval(close_bal) > 0) {
-        cell = row.insertCell();
-        cell.innerHTML = "";
-        cell = row.insertCell();
-        cell.innerHTML = `<strong>Closing balace:${abs(eval(close_bal))}</strong>`;
-    }
-
-    admin_button.remove();
-    reset_button.position(windowWidth / 2, table.size().height + 300);
-    httpGet(`${window.location.origin}/log.php?string=${encodeURIComponent(name_of_comp)}%20logged%20in`);
-    document.getElementById("dev").style.display = "none";
-
-    document.getElementById("loading").style.display = "none";
+document.getElementById("loading").style.display = "none";
 }
