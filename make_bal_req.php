fe991736bf4506df5d182afcac411ffa3cb52650
roborobo <kanhaagrawal0804@gmail.com>
2019-06-20 15:36:58 +0530

    cleaning php files

--- make_bal_req.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$string = ('C:\Users\Administrator\AppData\Local\Programs\Python\Python37\python.exe C:\inetpub\wwwroot\import_bal.py ');
-$out ="";
-$out = shell_exec($string);
-echo $out;
-?>
