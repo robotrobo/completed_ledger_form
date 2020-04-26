ad5133a755a9ec6ffbb7d4f9566a28de4c093e5c
robotrobo <kanhaagrawal0804@gmail.com>
2020-04-26 21:54:14 +0530

    whatsapp server update for closing balance/opening balance 2

--- whatsapp_node_server.js
+++ whatsapp_node_server.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const bodyParser = require('body-parser') 
 const { exec } = require('child_process')
+const mysql = require('mysql')
 const app = express()
 const port = 3000
+
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "9575024567",
+    database: "phone_numbers"
+  });
+
+function get_name(number){
+con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT comp from numbers where number=" + number, (err, result, fields) => {
+        if (err) throw err;
+        console.log(fields);
+        console.log(result);
+    })
+  })
+}
+  
 app.use(bodyParser.json())
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 app.post('/', (req, res) => {
@@ -11,9 +31,12 @@ app.post('/', (req, res) => {
         console.log(req)
         incoming = req.body.payload.payload["text"];
         in_Arr = incoming.split(" ");
-        keyword = in_Arr.shift()
-        
+        keyword = in_Arr.shift()  
         console.log(incoming);
+
+        phone_number = req.body.payload.sender.phone
+
+
     }
 res.statuscode = 200 
 res.send("hello")
