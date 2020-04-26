a0f8a5e1564288feb2aa7c41a37720d43d1e7730
robotrobo <kanhaagrawal0804@gmail.com>
2020-04-26 22:58:00 +0530

    finished bal

---
+++ whatsapp_server/get_balance.py
@@ -0,0 +1,27 @@
+import xml.etree.ElementTree as ET
+import  sys
+import requests
+
+if len(sys.argv) < 2 : 
+    print("please enter the mobile number to be searched")
+    exit(0)
+
+bal_req = '<ENVELOPE>  <HEADER>  <VERSION>1</VERSION>  <TALLYREQUEST>EXPORT</TALLYREQUEST>  <TYPE>COLLECTION</TYPE>  <ID>Remote Ledger Coll</ID>  </HEADER>  <BODY>  <DESC>  <STATICVARIABLES>  <SVEXPORTFORMAT>$$SysName:ASCII</SVEXPORTFORMAT>  </STATICVARIABLES>  <TDL>  <TDLMESSAGE>  <COLLECTION NAME="Remote Ledger Coll" ISINITIALIZE="Yes">  <TYPE>Ledger</TYPE>  <NATIVEMETHOD>Name</NATIVEMETHOD>  <NATIVEMETHOD>OpeningBalance </NATIVEMETHOD>  <NATIVEMETHOD>ClosingBalance </NATIVEMETHOD>  <NATIVEMETHOD>LedgerPhone </NATIVEMETHOD>  <NATIVEMETHOD>LedgerMobile </NATIVEMETHOD>  </COLLECTION>  </TDLMESSAGE>  </TDL>  </DESC> </BODY> </ENVELOPE>'
+
+r = requests.post("http://anishfoods.in:9000", data=bal_req)
+balance_string = r.text
+
+number = sys.argv[1]
+
+
+root = ET.fromstring(balance_string)
+
+collection = root[1][1][0]
+
+for ledger in collection.findall("LEDGER"):
+    mobile_element = ledger.find("LEDGERMOBILE") 
+    if mobile_element is not None:
+        if mobile_element.text == number:
+            print(ledger.find("CLOSINGBALANCE").text)
+
+
---
+++ whatsapp_server/whatsapp_node_server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const bodyParser = require('body-parser') 
+const { exec } = require('child_process')
+// const mysql = require('mysql')
+const app = express()
+const port = 3000
+
+// let con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "9575024567",
+//     database: "phone_numbers"
+//   });
+//   con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("connected to mysql server");
+
+//   })
+
+function get_name(number){
+    let comp = ""
+    con.query("SELECT comp from numbers where number=" + number, (err, result, fields) => {
+        if (err) throw err;
+        console.log(result);
+        comp = result[0]["comp"];
+    })
+
+    return comp;
+}
+  
+app.use(bodyParser.json())
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.post('/', (req, res) => {
+
+    if(req.body.type == 'message'){
+        // console.log(req)
+        incoming = req.body.payload.payload["text"];
+        in_Arr = incoming.split(" ");
+        keyword = in_Arr.shift()  
+        console.log(incoming);
+
+        phone_number = req.body.payload.sender.phone
+        console.log(phone_number)
+        if (phone_number.length == 12 && phone_number.startWith("91")){
+            phone_number = phone_number.substring(2)
+        }
+        // let comp_name = get_name(phone_number)
+        
+        if (keyword == "bal"){
+
+            exec(`py.exe get_balance.py ${phone_number}`,  (err, stdout, stderr)=>{
+
+                console.log(stdout)
+            })
+        }
+    }
+res.statuscode = 200 
+res.send("hello")
+
+})
+
+
+
+con.end()
