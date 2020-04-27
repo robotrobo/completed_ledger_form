ea197aa990b88ed9ed736afb147ec033b34d3ed3
robotrobo <kanhaagrawal0804@gmail.com>
2020-04-27 10:17:29 +0530

    yeah

--- whatsapp_server/whatsapp_node_server.js
+++ whatsapp_server/whatsapp_node_server.js
@@ -31,35 +31,29 @@ function get_name(number){
 app.use(bodyParser.json())
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 app.post('/', (req, res) => {
-    let strToSend = "";
     if(req.body.type == 'message'){
-        // console.log(req)
+        let strToSend = "";
+
         incoming = req.body.payload.payload["text"];
         in_Arr = incoming.split(" ");
-        keyword = in_Arr.shift()  
-        // console.log(incoming);
+        keyword = in_Arr.shift();  
 
         phone_number = req.body.payload.sender.phone
         if (phone_number.length == 12 && phone_number.startsWith("91")){
             phone_number = phone_number.substring(2)
         }
         console.log(phone_number)
-        // let comp_name = get_name(phone_number)
         console.log(keyword)
-        // let bal = ""
         if (keyword == "bal"){
-            console.log("doing this")
-            let bal = ""
             exec(`py.exe C:\\inetpub\\wwwroot\\whatsapp_server\\get_balance.py ${phone_number}`,  (err, stdout, stderr)=>{
                 if(err) console.log(err)
                 else if (stderr) console.log(stderr)
                 res.statusCode =200
-                strToSend = strToSend + stdout;
+                strToSend += stdout;
                 console.log(strToSend)
             })
         }
         res.statuscode = 200 
-        // res.text = "heldedelo"
         console.log(strToSend)
         res.send(strToSend)
     }
